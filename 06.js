1897ce49e7df3262a3ef445fba5da7b6e29380b4
teclare1028 <teclare1028@gmail.com>
2023-04-03 19:49:06 -0400

    first commit

---
+++ 06.js
@@ -0,0 +1,8 @@
+module.exports.items = ['item1', 'item2']
+const person = {
+    name: 'bob',
+    age: 23,
+    location: 'Maryland'
+}
+
+module.exports = {person}
---
+++ names.js
@@ -0,0 +1,6 @@
+const thomas = 'Thomas'
+const ryan = 'Ryan'
+const piper = 'Piper'
+const gavin = 'Gavin'
+
+module.exports = {thomas, ryan}
